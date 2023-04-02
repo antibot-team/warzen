5f7cf0f6a7a0c3be9be40e223e694a4daf5fe469
J_DDev <69683068+JayyDoesDev@users.noreply.github.com>
2023-04-02 13:35:05 -0400

    Formatting

--- src/Structures/Request.ts
+++ src/Structures/Request.ts
@@ -1,45 +1,46 @@
 import fetch from "cross-fetch";
 
 export class Request {
-    private api: string;
+  private api: string;
 
-    constructor() {
-        this.api = 'https://discordapp.com/api/webhooks';
+  constructor() {
+    this.api = "https://discordapp.com/api/webhooks";
+  }
+
+  async req(opt: RequestOptions): Promise<any> {
+    const request = {
+      method: opt.method,
+      headers: {
+        "Content-Type": "application/json",
+      },
     };
 
-    async req(opt: RequestOptions): Promise<any> {
+    const data = opt.data ? JSON.stringify(opt.data) : {};
 
-        const request = {
-            method: opt.method,
-            headers: {
-                "Content-Type": "application/json"
-            }
-        };
+    if (opt.method !== "GET") {
+      if (opt.method !== "DELETE") {
+        // @ts-ignore
+        request["body"] = data;
+      }
+    }
+    console.log(this.api + opt.endpoint);
 
-        const data = (opt.data) ? JSON.stringify(opt.data) : {};
-
-        if (opt.method !== "GET") {
-            if (opt.method !== "DELETE") {
-                // @ts-ignore
-                request["body"] = data;
-            };
-        };
-        console.log(this.api + opt.endpoint)
-
-        return new Promise(async (resolve, reject) => {
-            return fetch(this.api + opt.endpoint, request).then((x) => {
-                x.json().then((res) => {
-                    return resolve(res);
-                }).catch(() => {
-                    resolve(null);
-                });
-            });
-        });
-    };
-};
+    return new Promise(async (resolve, reject) => {
+      return fetch(this.api + opt.endpoint, request).then((x) => {
+        x.json()
+          .then((res) => {
+            return resolve(res);
+          })
+          .catch(() => {
+            resolve(null);
+          });
+      });
+    });
+  }
+}
 
 interface RequestOptions {
-    method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
-    endpoint: string;
-    data?: any;
-};
+  method: "GET" | "POST" | "PATCH" | "DELETE" | "PUT";
+  endpoint: string;
+  data?: any;
+}
--- src/Structures/WebhookClient.ts
+++ src/Structures/WebhookClient.ts
@@ -1,41 +1,44 @@
-import {Request} from "./Request";
-import {EmbedObject} from "../Typings/EmbedInterface";
+import { Request } from "./Request";
+import { EmbedObject } from "../Typings/EmbedInterface";
 
 export class WebhookClient {
-    public id: string;
-    public token: string;
-    public username?: string | null;
-    public avatar_url?: string | null;
-    private request: Request
-    constructor(ops = {
-        id: null as unknown as string,
-        token: null as unknown as string,
-        username: null as unknown as string | null,
-        avatar_url: null as unknown as string
-    }) {
-        this.id = ops.id;
-        this.token = ops.token;
-        this.username = ops.username;
-        this.avatar_url = ops.avatar_url;
-        this.request = new Request;
+  public id: string;
+  public token: string;
+  public username?: string | null;
+  public avatar_url?: string | null;
+  private request: Request;
+  constructor(
+    ops = {
+      id: null as unknown as string,
+      token: null as unknown as string,
+      username: null as unknown as string | null,
+      avatar_url: null as unknown as string,
+    }
+  ) {
+    this.id = ops.id;
+    this.token = ops.token;
+    this.username = ops.username;
+    this.avatar_url = ops.avatar_url;
+    this.request = new Request();
+  }
+
+  public async send(msg: string, embeds?: EmbedObject): Promise<void> {
+    let e: any[] = [];
+    if (embeds) {
+      e = [embeds];
     }
 
-    public async send(msg: string, embeds?: EmbedObject): Promise<void> {
-        let e: any[] = [];
-        if (embeds) {
-            e = [embeds]
-        }
-
-        if (typeof msg !=="string") throw new ReferenceError('msg must be a string')
-        await this.request.req({
-            method: 'POST',
-            endpoint: `/${this.id}/${this.token}`,
-            data: {
-                username: this.username,
-                avatar_url: this.avatar_url,
-                content: msg,
-                embeds: e
-            }
-        })
-    }
+    if (typeof msg !== "string")
+      throw new ReferenceError("msg must be a string");
+    await this.request.req({
+      method: "POST",
+      endpoint: `/${this.id}/${this.token}`,
+      data: {
+        username: this.username,
+        avatar_url: this.avatar_url,
+        content: msg,
+        embeds: e,
+      },
+    });
+  }
 }
